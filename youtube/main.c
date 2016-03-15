3ce1b3783ad9f40d9607f7cca56ca59c0c574842
Douglas G. Allen <kb9agt@gmail.com>
2016-03-14 23:05:14 -0500

    created cb projects

---
+++ youtube/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mylib.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("%i \n", AddInt(1, 2));
+    get_time_now();
+    return 0;
+}
---
+++ youtube/mylib.h
@@ -0,0 +1,9 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+
+
+#endif // MYLIB_H_INCLUDED
+
+int AddInt(int i1, int i2);
+void get_time_now();
